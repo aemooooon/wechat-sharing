f5ca15671a3de0aafd5d6e2d44775822c4c3d987
Hua Wangng <aemooooon@gmail.com>
2020-09-21 08:05:37 +0800

    init

---
+++ wechat/back.php
@@ -0,0 +1,29 @@
+<?php
+require_once "jssdk.php";
+$jssdk = new JSSDK("appid", "appsecret");
+
+header('Content-type: application/json');
+ob_start();
+
+$url = $_GET['url'];
+function CheckUrl($C_url){  
+    $str="/^http(s?):\/\/(?:[A-za-z0-9-]+\.)+[A-za-z]{2,4}(?:[\/\?#][\/=\?%\-&~`@[\]\':+!\.#\w]*)?$/";  
+    if (!preg_match($str,$C_url)){  
+        return false;  
+    }else{  
+    return true;  
+    }  
+}  
+if(!CheckUrl($url)) exit;
+
+$signPackage = $jssdk->GetSignPackage($url);
+
+$json['url'] = $signPackage["url"];
+$json['appid'] = $signPackage["appId"];
+$json['noncestr'] = $signPackage["nonceStr"];
+$json['timestamp'] = $signPackage["timestamp"];
+$json['signature'] = $signPackage["signature"];
+
+$json = json_encode($json);
+echo $json;
+?>
